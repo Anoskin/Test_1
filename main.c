b2a678a279b318a7406b809f7c4e45ddd64298df
Anoskin <50116181+Anoskin@users.noreply.github.com>
2021-12-30 12:00:49 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <malloc.h>
-#include <time.h>
+
 
 
 const unsigned short MAX_SIZE = 2; // фиксированная длинна очереди
@@ -10,7 +10,7 @@ typedef struct node { /*Создаем свой тип данных узел к�
     unsigned int val;
     struct node* next;
 } node_t;
- 
+
 typedef struct {  /*Создаем тип очередь с ссылкой на первый и последний узел*/
     node_t* head;
     node_t* tail;
@@ -18,19 +18,19 @@ typedef struct {  /*Создаем тип очередь с ссылкой на 
 } queue_t;
 
 //инициализация
-void queue_init(queue_t* q){
+void queue_init(queue_t* q) {
     q->head = q->tail = NULL;
     q->size = 0;
 }
 
 //добавить в очередь
-int queue_push(queue_t* q, unsigned int val){
-    if (q->size < MAX_SIZE){
+int queue_push(queue_t* q, unsigned int val) {
+    if (q->size < MAX_SIZE) {
         node_t* p = (node_t*)malloc(sizeof(node_t));
-        if(p != NULL){
-            p->val  = val;
+        if (p != NULL) {
+            p->val = val;
             p->next = NULL;
-            if(q->head == NULL)
+            if (q->head == NULL)
                 q->head = q->tail = p;
             else {
                 q->tail->next = p;
@@ -40,186 +40,143 @@ int queue_push(queue_t* q, unsigned int val){
         }
         return 1;
     }
-   return 0;
+    return 0;
 }
 
-size_t queue_size (queue_t* q){
+size_t queue_size(queue_t* q) {
 
-    if (q != NULL){
-      return q->size; 
+    if (q != NULL) {
+        return q->size;
     }
     return 0;
 }
 
 //чтение из очереди
-int queue_read (queue_t* q, unsigned int index,  unsigned int *val){
+int queue_read(queue_t* q, unsigned int index, unsigned int* val) {
+    node_t* p = q->head;
+    int i = 0;
+    if (queue_size(q) > index) {
+        for (; p != NULL; p= p->next) {
+            if (i == index) {
+                *val = p->val;
+                return 1;
+            }
+            ++i;
+        }
+            
+    }
 
-   if (queue_size(q) > index){
-        *val = (q->head + index)->val;
-        return 1;
-   }
-
-   return 0;
+    return 0;
 }
- 
- 
-int queue_empty(queue_t* q){
+
+
+int queue_empty(queue_t* q) {
     return (q->head == NULL);
 }
 
 
-static void read_two_queue (queue_t* q1, queue_t* q2){
-    unsigned int val;
-    unsigned int index;
-    printf("Enter index: \n");
-    scanf("%u", &index);
-    if (!queue_empty(q1) && queue_empty(q2)){
-        if (!queue_read(q1, index, &val)){
-            puts("index exceeds queue size");
-        }
-        else{
-            printf("%d", val);
-        }
+static void read_two_queue(queue_t* q, int index, unsigned int val) {
+    if (!queue_read(q, index, &val)) {
+        puts("index exceeds queue size");
     }
-    else if (queue_empty(q1) && queue_empty(q2)){
-        puts("queue is empty ");
+    else {
+        printf("%d", val);
     }
-    else if (queue_empty(q1) && !queue_empty(q2)){
-
-        if (!queue_read(q2, index, &val)){
-            puts("index exceeds queue size ");
-        }
-        else{
-            printf("%d", val);
-        }
-    }
-    else if (!queue_empty(q1) && !queue_empty(q2)){
-
-        if (queue_size(q1) + queue_size(q2) > index){
-
-           if (queue_size(q1) > index){
-               queue_read(q1, index, &val);
-               printf("%d", val);
-           }
-           else if (queue_size(q2) > index - queue_size(q1)){
-               queue_read(q1, index - queue_size(q1), &val);
-               printf("%d", val);
-           }
-           else{
-
-                 puts("index exceeds queue size ");
-           }
-       }
-   }
 }
 
-static void content(queue_t* q){
+static void content(queue_t* q) {
     node_t* p = q->head;
-    if (!queue_empty(q)){
-        while (p != NULL){
-        
+    if (!queue_empty(q)) {
+        while (p != NULL) {
             printf("%u ", p->val);
             p = p->next;
         }
-            printf("\n");
-       }
-    else{
-    puts("queue is empty ");
-   }
+        printf("\n");
+    }
+    else {
+        puts("queue is empty ");
+    }
 }
 
-int queue_union(queue_t* q3, queue_t* q1, queue_t* q2){
-    node_t* p1, *p2, *eq, **pq;
+int queue_union(queue_t* q3, queue_t* q1, queue_t* q2) {
+    node_t* p1, * p2, * eq, ** pq;
     p1 = q1->head;
     p2 = q2->head;
     pq = &q3->head;
     eq = q3->head;
 
-    if((q1->head == NULL) || (q2->head == NULL)){
+    if ((q1->head == NULL) || (q2->head == NULL)) {
         printf("one of the queues is empty \n");
         return 0;
     }
     while ((p1 != NULL) && (p2 != NULL))
     {
-      *pq = p1;
-      eq = *pq;
-      pq  = &(*pq)->next;
-      *pq = p2;
-      eq = *pq;
-      pq  = &(*pq)->next; 
-      p1  = p1->next;
-      p2  = p2->next;
-    }
-    while(p1 != NULL){
         *pq = p1;
-        p1  = p1->next;
-        eq  = *pq;
-        pq  = &(*pq)->next;
-    }
- 
-    while(p2 != NULL){
+        p1 = p1->next;
+        eq = *pq;
+        pq = &(*pq)->next;
         *pq = p2;
-        p2  = p2->next;
-        eq  = *pq;
-        pq  = &(*pq)->next;
+        p2 = p2->next;
+        eq = *pq;
+        pq = &(*pq)->next;
+    }
+    while (p1 != NULL) {
+        *pq = p1;
+        p1 = p1->next;
+        eq = *pq;
+        pq = &(*pq)->next;
+    }
+
+    while (p2 != NULL) {
+        *pq = p2;
+        p2 = p2->next;
+        eq = *pq;
+        pq = &(*pq)->next;
     }
     q3->tail = eq;
+    q3->size = queue_size(q1) + queue_size(q2);
     return 1;
 }
 
 
-int timeout ( int seconds )
-{
-    clock_t endwait;
-    endwait = clock () + seconds * CLOCKS_PER_SEC ;
-    while (clock() < endwait) {}
+static void get_queue(queue_t* q1, queue_t* q2, queue_t* q3, unsigned int val) {
 
-    return  1;
-}
 
-static void get_queue (queue_t* q1, queue_t* q2, queue_t* q3){
-
-    puts("Enter number queue: ");
-    unsigned int val = 0;
-    scanf("%u", &val);
-    if (val == 1){
+    if (val == 1) {
         content(q1);
     }
-    else if (val == 2){
+    else if (val == 2) {
 
         content(q2);
     }
-    else if (val == 3){
+    else if (val == 3) {
 
         content(q3);
     }
-    else{
+    else {
 
         fprintf(stderr, "Incorrect queue number entered \n");
     }
 }
 
-static void get_size (queue_t* q1, queue_t* q2, queue_t* q3){
+static void get_size(queue_t* q1, queue_t* q2, queue_t* q3, unsigned int val) {
 
     unsigned int size = 0;
-    puts("Enter number queue: ");
-    unsigned int val = 0;
-    scanf("%u", &val);
-    if (val == 1){
+    if (val == 1) {
         size = (unsigned int)queue_size(q1);
-        printf ("size = %u \n", size);
+        printf("size = %u \n", size);
     }
-    else if (val == 2){
+    else if (val == 2) {
 
         size = (unsigned int)queue_size(q1);
-        printf ("size = %u \n", size);
+        printf("size = %u \n", size);
     }
-    else if (val == 3){
+    else if (val == 3) {
 
         size = (unsigned int)queue_size(q1);
-        printf ("size = %u \n", size);
+        printf("size = %u \n", size);
     }
-    else{
+    else {
 
         fprintf(stderr, "Incorrect queue number entered \n");
     }
@@ -227,62 +184,82 @@ static void get_size (queue_t* q1, queue_t* q2, queue_t* q3){
 }
 
 
-void queue_pop(queue_t* q){
+void queue_pop(queue_t* q) {
     node_t* t = q->head;
-    if(t != NULL){
+    if (t != NULL) {
         q->head = q->head->next;
         free(t);
     }
-    if(q->head == NULL)
+    if (q->head == NULL)
         q->tail = NULL;
 }
 
-int main (void){
+int main(void) {
 
-   char command;
-   unsigned int val = 0;
-   queue_t q1, q2, q3;
-   queue_init(&q1);
-   queue_init(&q2);
-   queue_init(&q3);
+    char command;
+    unsigned int val = 0;
+    unsigned int index;
+    queue_t q1, q2, q3;
+    queue_init(&q1);
+    queue_init(&q2);
+    queue_init(&q3);
 
-   for (;;){
+    for (;;) {
         puts("Enter command :");
         command = getchar();
-        if (command!= '\n' && command != EOF){
-            if (command == 'r'){ // Чтение из очереди + дополнительный параметр индекс 
-                read_two_queue(&q1, &q2);
-            }
-            else if (command == 'w'){  // Добавления значения в очередь дополнительный пармаметр число которое надо добавить в очередь
-                puts("Enter number: ");
-                scanf("%u", &val);
-                if (!queue_push(&q1, val)){
-                   if (!queue_push(&q2, val)){
-                      printf("size larger %u \n", MAX_SIZE);
-                   }
+        if (command != '\n' && command != EOF) {
+            if (command == 'r') { // Чтение из очереди 
+                puts("Enter number queue: ");
+                scanf_s("%u", &val);
+                printf("Enter index: \n");
+                scanf_s("%u", &index);
+                if (val == 1) {
+                    read_two_queue(&q1, index, val);
+                }
+                else if (val == 2) {
+                    read_two_queue(&q2, index, val);
+                }
+                else if (val == 3) {
+                    read_two_queue(&q3, index, val);
+                }
+                else {
+                    puts("there is no queue");
                 }
             }
-            else if (command == 's'){ // размер очереди плюс содержимое всей очереди
-                get_size(&q1, &q2, &q3);
-                get_queue(&q1, &q2, &q3);
+            else if (command == 'w') {  // Добавления значения в очередь 
+                puts("Enter number: ");
+                scanf_s("%u", &val);
+                if (!queue_push(&q1, val)) {
+                    if (!queue_push(&q2, val)) {
+                        printf("size larger %u \n", MAX_SIZE);
+                    }
+                }
             }
-            else if (command ==  'c'){ // содержимое очереди
-                get_queue(&q1, &q2, &q3);
+            else if (command == 's') { // размер очереди плюс содержимое всей очереди
+                puts("Enter number queue: ");
+                scanf_s("%u", &val);
+                get_size(&q1, &q2, &q3, val);
+                get_queue(&q1, &q2, &q3, val);
             }
-            else if (command ==  'u'){// сличяние очереди в шахматном порядке
-                queue_union (&q3, &q1, &q2);
+            else if (command == 'c') { // содержимое очереди
+                puts("Enter number queue: ");
+                scanf_s("%u", &val);
+                get_queue(&q1, &q2, &q3, val);
             }
-            else if (command ==  'q'){
+            else if (command == 'u') {// слияние очереди в шахматном порядке
+                queue_union(&q3, &q1, &q2);
+            }
+            else if (command == 'q') {
                 break;
             }
-            else{
+            else {
                 puts("Unknown commands");
             }
         }
         getchar();
-   }
-   queue_pop(&q1);
-   queue_pop(&q2);
-   queue_pop(&q3);
-   return 0;
+    }
+    queue_pop(&q1);
+    queue_pop(&q2);
+    queue_pop(&q3);
+    return 0;
 }
